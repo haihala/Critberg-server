7ae43d6037eeeddea9b9e378d25b380e5cbf097e
haihala <eero.haihala@gmail.com>
2018-05-10 10:02:00 +0300

    made networking into a thread

--- main.py
+++ main.py
@@ -13,16 +13,22 @@ import sys
 
 def main(args):
     args = parse_args(args)
-    network_handle = Network(args["networking"])
-    engine = Master_engine(network_handle, args)
 
+    network_handle = Network(args["networking"])
+    network_thread = threading.Thread(target=network_handle.loop)
+    network_thread.start()
+
+    engine = Master_engine(network_handle, args)
     engine_thread = threading.Thread(target=engine.loop)
     engine_thread.start()
 
     while True:
         user_input = input(">").strip().lower()
         if user_input == "stop":
+            # To gracefully shut down the thread, use this approach.
+            # After the loops in loop methods, implement everything required for a graceful shutdown.
             engine.running = False
+            network_handle.running = False
             break
 
 if __name__ == '__main__':
--- util/networking.py
+++ util/networking.py
@@ -4,4 +4,14 @@ Abstraction of the networking. Used to parse packages to usable format, etcetera
 """
 class Network(object):
     def __init__(self, kwargs):
+        self.running = True
+
+        self._inbox = {}
+        # Inbox is a container for unread messages.
+        # Key is the user sending the messages, value is a list of unreads.
+        # Supposed to be accessed through helper methods.
+
+        # Todo: Bind ports and get ready to loop
+
+    def loop(self):
         pass
