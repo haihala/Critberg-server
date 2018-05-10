7057228428465ce8a7e4f8f925bdcb7d246ba008
Henrik Moilanen <henrikmoilanen@gmail.com>
2018-05-10 14:16:49 +0300

    Initial base for player object

--- gameobjects/player.py
+++ gameobjects/player.py
@@ -2,3 +2,20 @@
 Represents the players of the game.
 
 """
+
+from .gameobject import GameObject
+
+class Player(object):
+    """
+    Adding basic player attributes regarding health, hand and manapool
+    """
+    def __init__(self):
+        super(self)
+        self.health = 0
+        self.resource = {}
+        self.hand = []
+        self.deck = []
+        self.AZone = []
+        self.DZone = []
+        self.graveyard = []
+        self.exile = []
