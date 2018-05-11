7b3de5dc214d989dec61018430fe4f968ee71fbc
Eero Häihälä <eero.haihala@gmail.com>
2018-05-11 20:28:34 +0300

    made lookup return an empty card and take an art parameter as optional

--- engine/card_lookup.py
+++ engine/card_lookup.py
@@ -3,7 +3,8 @@ Will be used to lookup cards from IDs.
 
 """
 
-def lookup(cardID, artID):
-    return 4    # chosen by fair dice roll
-                # guaranteed to be random
-                #           - xkcd.com/221
+from gameobjects.card import Card
+
+def lookup(cardID, artID=0):
+    # TODO
+    return Card()
