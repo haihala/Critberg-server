b537b5e971b5a3dbd03f0e64277333486ada4f72
Eero Häihälä <eero.haihala@gmail.com>
2018-05-11 13:52:59 +0300

    made lookup take extra parameter for art

--- engine/card_lookup.py
+++ engine/card_lookup.py
@@ -3,7 +3,7 @@ Will be used to lookup cards from IDs.
 
 """
 
-def lookup(ID):
+def lookup(cardID, artID):
     return 4    # chosen by fair dice roll
                 # guaranteed to be random
                 #           - xkcd.com/221
